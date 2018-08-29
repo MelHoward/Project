2d3b454a201f38dea26c83198246dc1c80ff21f9
Mel Howard <howardmel98@gmail.com>
2018-08-28 19:50:41 -0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {       
+            [System.STAThreadAttribute()]
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.0.0.0")]
+
+            public static void Main()
+            {
+                Project.App app = new Project.App();
+                app.InitializeComponent();
+                app.Run();
+            }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
