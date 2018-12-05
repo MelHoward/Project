5d68d8852e5e2c80bc56c86bc8d80ce4fd884a37
Mel Howard <melshowa@ut.utm.edu>
2018-12-04 19:28:54 -0600

    Add files via upload

---
+++ Engine/Random.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace twoDTDS.Engine
+{
+    /*---------------------------------------------------------------------------------------
+                                        RANDOM
+    ---------------------------------------------------------------------------------------*/
+    public class Random
+    {
+        System.Random rand = new System.Random((int)DateTime.UtcNow.TimeOfDay.TotalMilliseconds);
+
+        /*============================= NextDouble ===========================*/
+        public double NextDouble()
+        {
+            return rand.NextDouble();
+        }
+
+        /*============================= NextDouble ===========================*/
+        public double NextDouble(double min, double max)
+        {
+            return (double)rand.Next((int)(min * 10000), (int)(max * 10000)) / 10000;
+        }
+
+        /*============================= Next ===========================*/
+        public int Next(int min, int max)
+        {
+            return rand.Next(min, max);
+        }
+    }
+}
---
+++ Game/CNormBullet.cs
@@ -0,0 +1,15 @@
+﻿using twoDTDS.Engine;
+
+namespace twoDTDS.Game
+{
+
+    /*---------------------------------------------------------------------------------------
+                                   << ENEMYAMMO >> : GAMEOBJECT
+    ---------------------------------------------------------------------------------------*/
+    public abstract class EnemyAmmo : GameObject
+    {
+        public EnemyAmmo(Map map) : base(map) { }
+        public int Damage { get; set; } = 1;
+    }
+
+}
