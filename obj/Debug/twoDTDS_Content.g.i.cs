11bb7ec21e0b9d2fcfcef05109d8c5f5efe1efae
Mel Howard <howardmel98@gmail.com>
2018-12-05 16:23:25 -0600

    Added sprites

--- obj/Debug/twoDTDS_Content.g.i.cs
+++ obj/Debug/twoDTDS_Content.g.i.cs
@@ -8,10 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/1.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/2.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/3.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/4.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/5.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/stage2.png")]
 
 
