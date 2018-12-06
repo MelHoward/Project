39b3c54aa15eb432418892b9cac6066d12473a8d
ColeDavis213 <coledavi@ut.utm.edu>
2018-12-05 23:39:51 -0600

    Player changes direction

--- Game/Player.cs
+++ Game/Player.cs
@@ -37,6 +37,9 @@ namespace twoDTDS.Game
                       dyingSize = 40;
         public string uri;
         public bool invincible = false;
+        public bool roll = false;
+        GameObject iteration;
+        Obstacle Obst;
 
         /*============================= Player >> CTOR ===========================*/
         public Player(Map map) : base(map)
@@ -99,14 +102,23 @@ namespace twoDTDS.Game
 
                 X = Math.Min(Map.Width - Width, Math.Max(0, X));
                 Y = Math.Min(Map.Height - Height, Math.Max(0, Y));
-                IsPlayerHit();
-                if (Keyboard.IsKeyDown(Key.E)) {  Roll();  }
+
+                if (Keyboard.IsKeyDown(Key.E))
+                {
+                    Roll();
+                }
                 RollReset();
+
+                IsPlayerHit();
+                ShowIframes();
+
+
                 CheckPowerUp();
             }
         }
-
-        /*================================== Roll =============================*/
+        /// <summary>
+        /// Makes it to where you are invincible
+        /// </summary>
         private void Roll()
         {
             if (rollFrames < 50 && rollFrames > 0)
@@ -114,46 +126,106 @@ namespace twoDTDS.Game
                 if (rollFrames >= 0 && invincible != true)
                 {
                     rollFrames = 0;
+                    roll = true;
                 }
-                invincible = true;
-                Sprite = new Rec(30, 35, uri);
+                
+                if (roll == true)
+                {
+                    invincible = true;
+                    Sprite = new Rec(30, 35, uri);
+                }
+                else
+                {
+                    invincible = false;
+                }
+                
             }
         }
-
-        /*================================== RollReset =============================*/
+        /// <summary>
+        /// Makes you not invincible after a short time
+        /// </summary>
         private void RollReset()
         {
             if (rollFrames == 50)
             {
                 invincible = false;
-                Sprite = new Rec(40, 45, uri);
+                Sprite = new Rec(Width, Height, uri);
                 rollFrames = -25;
             }
+
             rollFrames++;
         }
 
-        /*================================== Move =============================*/
+        private void ShowIframes()
+        {
+            if (invincibilityFrames > 0)
+            {
+                invincible = true;
+                invincibilityFrames--;
+            }
+            if (invincibilityFrames == 0)
+            {
+                invincible = false;
+            }
+            if (invincible == true)
+            {
+                if (invincibilityFrames % 2 == 0)
+                {
+                    Sprite = null;
+                }
+                else
+                {
+                    Sprite = new Rec(40, 45, uri);
+                }
+            }
+        }
+        /// <summary>
+        /// Lets you move using WASD
+        /// </summary>
         private void Move()
         {
+            for (int i = 0; i < Map.Objects.Count; i++)
+            {
+                iteration = Map.Objects[i];
+                if (!iteration.ObDied && iteration is Obstacle)
+                {
+                    Obst = (Obstacle)iteration;
+                    if (IsHit(this, Obst))
+                    {
+                        Obst.CollisionSetBack(this);
+                        Obst.checkTopBot(this);
+                    }
+                }
+            }
             if (Keyboard.IsKeyDown(Key.A))
             {
+                uri = Asset.hero[3];
+                Sprite = new Rec(Width, Height, uri);
                 X -= speed;
             }
             else if (Keyboard.IsKeyDown(Key.D))
             {
+                uri = Asset.hero[4];
+                Sprite = new Rec(Width, Height, uri);
                 X += speed;
             }
             if (Keyboard.IsKeyDown(Key.W))
             {
+                uri = Asset.hero[0];
+                Sprite = new Rec(Width, Height, uri);
                 Y -= speed;
             }
             else if (Keyboard.IsKeyDown(Key.S))
             {
+                uri = Asset.hero[2];
+                Sprite = new Rec(Width, Height, uri);
                 Y += speed;
             }
-        }
 
-        /*================================== Shoot =============================*/
+        }
+        /// <summary>
+        /// Lets you shoot using the arrow keys
+        /// </summary>
         private void Shoot()
         {
             if (bulletCreate == null)
@@ -180,13 +252,31 @@ namespace twoDTDS.Game
                     {
                         a.direction = "right";
                     }
+                    if (Keyboard.IsKeyDown(Key.Left) && Keyboard.IsKeyDown(Key.Up))
+                    {
+                        a.direction = "upLeft";
+                    }
+                    if (Keyboard.IsKeyDown(Key.Left) && Keyboard.IsKeyDown(Key.Down))
+                    {
+                        a.direction = "downLeft";
+                    }
+                    if (Keyboard.IsKeyDown(Key.Right) && Keyboard.IsKeyDown(Key.Up))
+                    {
+                        a.direction = "upRight";
+                    }
+                    if (Keyboard.IsKeyDown(Key.Right) && Keyboard.IsKeyDown(Key.Down))
+                    {
+                        a.direction = "downRight";
+                    }
+
                     Map.AddObject(a);
                 };
-            }
+            };
             bulletCreate.Start();
         }
-
-        /*================================== IsPlayerHit =============================*/
+        /// <summary>
+        /// Detects when you get hit and shakes "camera" when you do
+        /// </summary>
         private void IsPlayerHit()
         {
             foreach (GameObject obj in Map.Objects)
@@ -198,15 +288,6 @@ namespace twoDTDS.Game
                         if (invincible == false)
                         {
                             invincibilityFrames = 50;
-                            if (invincibilityFrames > 0)
-                            {
-                                invincible = true;
-                                invincibilityFrames--;
-                            }
-                            if (invincibilityFrames == 0)
-                            {
-                                invincible = false;
-                            }
 
                             myScore.PlayerHit(10);
 
@@ -236,28 +317,32 @@ namespace twoDTDS.Game
             }
         }
 
-        /*================================== CheckPowerUp =============================*/
         public void CheckPowerUp()
         {
             InvincibilityPowerUp speed = new InvincibilityPowerUp(Map, this, X + 100, Y - 100);
             if (Keyboard.IsKeyDown(Key.Space))
             {
+
+
                 Map.AddObject(speed);
             }
+            if (Keyboard.IsKeyDown(Key.D3))
+            {
+                Rock rock = new Rock(Map, 300, 300);
+                Map.AddObject(rock);
+            }
+
         }
 
         /*================================== OnRender =============================*/
         public override void OnRender(DrawingContext dc)
         {
-            if (!myScore.Died)
-            {
-                base.OnRender(dc);
-            }
+            if (!myScore.Died) { base.OnRender(dc); }
             else
             {
                 Map.DrwTxt(dc, "YOU DIED", (Map.Width / 2), (Map.Height / 2),
-                    dyingSize, HorizontalAlignment.Center,
-                    VerticalAlignment.Center);
+                             dyingSize, HorizontalAlignment.Center,
+                             System.Windows.VerticalAlignment.Center);
             }
         }
     }
